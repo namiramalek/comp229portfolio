d05dd2086d9725703e2fe50567794f2261c5f2c3
Namira Malek <namiramalek234@gmail.com>
2021-02-28 02:47:19 +0530

    updated uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/business_store"
-}
+    //"URI": "mongodb://localhost/business_store"
+   "URI": "mongodb+srv://namira:SWJBrKAd8AlSxvKV@databaseserver.9d4sb.mongodb.net/business_store?retryWrites=true&w=majority"
+   }
